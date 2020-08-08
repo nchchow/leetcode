03b2963db48182e98f686251860089bf01c93992
nelson chow <nelsonchow91.nc@gmail.com>
2020-08-07 17:20:28 -0700

    remove-duplicates-sorted-array

--- remove-duplicates-sorted-array/remove-duplicates-from-sorted-arrays.js
+++ remove-duplicates-sorted-array/remove-duplicates-from-sorted-arrays.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-const removeDuplicates = function (nums) {
+const removeDuplicates = (nums) => {
   let j = 0;
   for (let i = 1; i < nums.length; i++) {
     if (nums[j] !== nums[i]) {
