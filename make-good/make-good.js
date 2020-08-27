74182580e7a320ae4f9936b87da3c3c4b4d935a5
nelson chow <nelsonchow91.nc@gmail.com>
2020-08-27 13:10:05 -0700

    make-good

--- make-good/make-good.js
+++ make-good/make-good.js
@@ -6,10 +6,7 @@ const makeGood = (s) => {
   const removeBad = (s) => {
     for (let i = 0; i < s.length - 1; i++) {
       if (Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1)) === 32) {
-        const arr = s.split("");
-        arr.splice(i, 2);
-        s = arr.join("");
-        return removeBad(s);
+        s = removeBad(s.replace(s[i] + s[i + 1], ""));
       }
     }
     return s;
