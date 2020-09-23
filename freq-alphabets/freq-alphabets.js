5dfcca18fa98e4eb08d6bfdc7ff1089cdae8779f
nelson chow <nelsonchow91.nc@gmail.com>
2020-09-23 10:50:04 -0700

    freq-alphabets

--- freq-alphabets/freq-alphabets.js
+++ freq-alphabets/freq-alphabets.js
@@ -3,7 +3,7 @@
  * @return {string}
  */
 const freqAlphabets = (s) => {
-  const toChar = (char) => String.fromCharCode(parseInt(char) + 96);
+  const toChar = (charCode) => String.fromCharCode(parseInt(charCode) + 96);
   let res = "";
   for (let i = s.length - 1; i >= 0; i--) {
     if (s[i] === "#") {
