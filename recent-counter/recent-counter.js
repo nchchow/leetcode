514d137cf48bd4a38249c8bda183a23ebb6131e9
nelson chow <nelsonchow91.nc@gmail.com>
2020-09-28 18:45:07 -0700

    recent-counter

---
+++ recent-counter/recent-counter.js
@@ -0,0 +1,21 @@
+const RecentCounter = function () {
+  this.requests = [];
+};
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function (t) {
+  this.requests.push(t);
+  while (t - this.requests[0] > 3000) {
+    this.requests.shift();
+  }
+  return this.requests.length;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
