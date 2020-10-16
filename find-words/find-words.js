4d4bc8c14c2848b7b010e1c8e4425a80e0b45d96
nelson chow <nelsonchow91.nc@gmail.com>
2020-10-16 12:05:15 -0700

    find-words

--- find-words/find-words.js
+++ find-words/find-words.js
@@ -7,14 +7,12 @@ const findWords = (words) => {
   const m = new Set(["a", "s", "d", "f", "g", "h", "j", "k", "l"]);
   const b = new Set(["z", "x", "c", "v", "b", "n", "m"]);
 
-  return words.reduce((out, word) => {
+  return words.filter((word) => {
     const chars = word.toLowerCase().split("");
-    if (
+    return (
       chars.every((c) => t.has(c)) ||
       chars.every((c) => m.has(c)) ||
       chars.every((c) => b.has(c))
-    )
-      out.push(word);
-    return out;
-  }, []);
+    );
+  });
 };
